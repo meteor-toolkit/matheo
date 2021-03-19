d0b53b30add9e4167384fabb9b758a6e48128db8
kavya.jagan <kavya.jagan@npl.co.uk>
2021-03-19 13:25:51 +0000

    updated to include inputs to functions rather than class

--- matheo/linear_algebra/tensor_product.py
+++ matheo/linear_algebra/tensor_product.py
@@ -20,31 +20,30 @@ __status__ = "Development"
 
 
 class TensorProduct:
-    def __init__(self, Q, x):
-        self.Q = Q
-        self.x = x
+    def __init__(self):
+        pass
 
-    def kronmult(self):
+    def kronmult(self, x, Q):
         """
         Function to evaluate the product of a matrix with a tensor/kronecker product of matrices
 
-        :param X: matrix or vector
+        :param x: matrix or vector
         :param Q: matrices that need to be "tensor producted" - Q1, Q2, ...
-        :return: (Q1 kron Q2 kron ...)*X
+        :return: (Q1 kron Q2 kron ...)*x
 
         Created: 08-02-2021
         """
 
-        N = len(self.Q)
+        N = len(Q)
         n = np.zeros(N)
         nright = 1
         nleft = 1
 
         for i in range(N-1):
-            n[i] = self.Q[i].shape[0]
+            n[i] = Q[i].shape[0]
             nleft = int(nleft*n[i])
 
-        n[N-1] = self.Q[N-1].shape[0]
+        n[N-1] = Q[N-1].shape[0]
 
         for i in range(N-1, -1, -1):
             base = 0
@@ -56,17 +55,17 @@ class TensorProduct:
                     index1 = int(index1)
                     index2 = int(index2)
                     nright = int(nright)
-                    if self.Q[i].ndim == 2:
-                        self.x[index1:index2:nright] = np.matmul(self.Q[i], self.x[index1:index2:nright])
-                    elif self.Q[i].ndim == 1:
-                        tclass = Toeplitz(self.x[index1:index2:nright], self.Q[i])
-                        self.x[index1:index2:nright] = tclass.toepfftmult()
+                    if Q[i].ndim == 2:
+                        x[index1:index2:nright] = np.matmul(Q[i], x[index1:index2:nright])
+                    elif Q[i].ndim == 1:
+                        tclass = Toeplitz()
+                        x[index1:index2:nright] = tclass.toepfftmult(x[index1:index2:nright], Q[i])
                     else:
                         sys.exit('Funky dimension of Q!')
                 base = base + jump
-            nleft = nleft/n[np.max(i-1, 0)]
-            nright = nright*n[i]
+            nleft = int(nleft/n[np.max(i-1, 0)])
+            nright = int(nright*n[i])
 
-        X = self.x
+        X = x
         return X
 
